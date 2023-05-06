b6f6b098772efafc261162a592bd568e202f6050
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-06 15:13:23 +0200

    Make third assignment, mouse events on a picture that shows or hide it's description text

--- index.js
+++ index.js
@@ -27,9 +27,14 @@ const createItem = (name, father, nameOfClasses, listItems, nameClass, content, 
       father.appendChild(item);
 
     } else if (name === 'img') {
+      const figure = document.createElement('figure');
       const item = document.createElement(name);
-      item.setAttibute(attributeName, attibuteValue)
-      father.appendChild(item);
+      item.setAttribute(attributeName, attibuteValue)
+      const descrip = document.createElement('figcaption');
+      descrip.innerHTML = content;
+      figure.appendChild(item);
+      figure.appendChild(descrip);
+      father.appendChild(figure);
 
     } else {
       const item = document.createElement(name);
@@ -39,7 +44,7 @@ const createItem = (name, father, nameOfClasses, listItems, nameClass, content, 
     }
 }
 
-//---Set Html elements const createItem(name, father, nameOfClasses, listItems, nameClass, content='', attributeName='', attibuteValue='')
+//---> Set Html elements const createItem(name, father, nameOfClasses, listItems, nameClass, content='', attributeName='', attibuteValue='')
 let mainClassName = ['container'];
 let sectionsClassName = ['list', 'toggleText', 'imageEfect'];
 let twoItems = ['hacer katas sin parar', 'Seguir aprendiendo']
@@ -49,11 +54,11 @@ const button = document.querySelector('button');
 createItem('main', body, mainClassName);
 createItem('section', document.querySelector('.container'), sectionsClassName);
 
-//---> First assignment: 1 <ul></ul> with 2 <li></li>
+//1. ---> First assignment: 1 <ul></ul> with 2 <li></li>
 createItem('h1', document.querySelector('.list'), [], [], '', 'Lista de tareas: ');
 createItem('ul', document.querySelector('.list'), [], twoItems);
 
-//---> Second assignment: 1 button, 1 text and a click event that show or hide the text
+//2. ---> Second assignment: 1 button, 1 text and a click event that show or hide the text
 createItem('button', document.querySelector('.toggleText'), [], [], '', 'Click me');
 createItem('p', document.querySelector('.toggleText'), [], [], 'parag', 'El cerebro humano es asombroso, Funciona 24/7 desde que naces y sólo se detiene cuando vas a escribir código.');
 const btn = document.querySelector('button');
@@ -61,3 +66,13 @@ const parag = document.querySelector('.parag');
 
 const handlerClick = () => parag.classList.toggle('hidden');
 btn.addEventListener('click', handlerClick);
+
+//3. ---> third assignment: mouseover and mouseout event on a picture that shows or hide a figcaption
+createItem('img', document.querySelector('.imageEfect'), [], [], '', 'Mapaches con gafas de sol', 'src', './assets/images/mapaches.webp');
+
+const figure = document.querySelector('figure');
+const figureDescrip = document.querySelector('figure > figcaption');
+
+const handlerMouseEvent = () =>  figureDescrip.classList.toggle('hidden');
+figure.addEventListener('mouseover', handlerMouseEvent);
+figure.addEventListener('mouseout', handlerMouseEvent);
