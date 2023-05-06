02e80cc3de805b8a4fb2d96c4e269c3cfe8fcab5
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-06 14:29:41 +0200

    Make second assignment, button that show or hide a paragraph

--- index.js
+++ index.js
@@ -39,12 +39,25 @@ const createItem = (name, father, nameOfClasses, listItems, nameClass, content, 
     }
 }
 
-//---Set Html elements const createItem = (name, father, nameOfClasses, listItems, nameClass, content='', attributeName='', attibuteValue='')
+//---Set Html elements const createItem(name, father, nameOfClasses, listItems, nameClass, content='', attributeName='', attibuteValue='')
 let mainClassName = ['container'];
 let sectionsClassName = ['list', 'toggleText', 'imageEfect'];
 let twoItems = ['hacer katas sin parar', 'Seguir aprendiendo']
 
+const button = document.querySelector('button');
+//main container and sections
 createItem('main', body, mainClassName);
 createItem('section', document.querySelector('.container'), sectionsClassName);
+
+//---> First assignment: 1 <ul></ul> with 2 <li></li>
 createItem('h1', document.querySelector('.list'), [], [], '', 'Lista de tareas: ');
 createItem('ul', document.querySelector('.list'), [], twoItems);
+
+//---> Second assignment: 1 button, 1 text and a click event that show or hide the text
+createItem('button', document.querySelector('.toggleText'), [], [], '', 'Click me');
+createItem('p', document.querySelector('.toggleText'), [], [], 'parag', 'El cerebro humano es asombroso, Funciona 24/7 desde que naces y sólo se detiene cuando vas a escribir código.');
+const btn = document.querySelector('button');
+const parag = document.querySelector('.parag');
+
+const handlerClick = () => parag.classList.toggle('hidden');
+btn.addEventListener('click', handlerClick);
