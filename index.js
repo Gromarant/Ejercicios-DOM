d6e4a59af43d75f542477c0b8a0510719f4c5950
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-06 09:48:33 +0200

    Make first assignment, ul element with two li

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-let mainClassName = ['container'];
-let sectionsClassName = ['list', 'toggleText', 'imageEfect'];
 const body = document.querySelector('body');
 
-const createItem = (name, father, nameOfClasses, listItems, nameClass, content='', attributeName='', attibuteValue='') => {
+//---> Constructor function
+const createItem = (name, father, nameOfClasses, listItems, nameClass, content, attributeName='', attibuteValue='') => {
 
-  if ((name === 'main' || name === 'section') && nameOfClasses.length > 0 ) {
+  if ((name === 'main' || name === 'section') && nameOfClasses !== []) {
 
     for (let i=0; i<nameOfClasses.length; i++) {
-        const section = document.createElement(name);
-        section.className = nameOfClasses[i];
-        father.appendChild(section)
+      const section = document.createElement(name);
+      section.className = nameOfClasses[i];
+      father.appendChild(section)
+    
     }
   } else if (name === 'ul' && listItems !== []) {
       const list = document.createElement(name);
-      list.innerHTML = content;
 
       for (let i=0; i<listItems.length; i++) {
-        const item = document.createElement(name);
+        const item = document.createElement('li');
         item.innerHTML = listItems[i];
         list.appendChild(item);
       }
@@ -39,4 +38,13 @@ const createItem = (name, father, nameOfClasses, listItems, nameClass, content='
       father.appendChild(item);
     }
 }
+
+//---Set Html elements const createItem = (name, father, nameOfClasses, listItems, nameClass, content='', attributeName='', attibuteValue='')
+let mainClassName = ['container'];
+let sectionsClassName = ['list', 'toggleText', 'imageEfect'];
+let twoItems = ['hacer katas sin parar', 'Seguir aprendiendo']
+
 createItem('main', body, mainClassName);
+createItem('section', document.querySelector('.container'), sectionsClassName);
+createItem('h1', document.querySelector('.list'), [], [], '', 'Lista de tareas: ');
+createItem('ul', document.querySelector('.list'), [], twoItems);
